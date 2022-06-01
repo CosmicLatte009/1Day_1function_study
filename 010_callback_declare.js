f4f6e3d2230a890a10402f7e69601c44c286fad8
CosmicLatte009 <ohhm009@gmail.com>
2022-06-01 22:55:06 +0900

    add comments

--- 010_callback_declare.js
+++ 010_callback_declare.js
@@ -1,3 +1,4 @@
+//콜백함수(1): 선언적 함수 사용하기
 // 함수를 선언 합니다.
 function callFiveTimes(callback) {
     for(let i = 0; i < 5; i++) {
@@ -9,4 +10,5 @@ function print(i) {
     console.log(i)
 }
 
+//함수를 호출합니다.
 callFiveTimes(print);
