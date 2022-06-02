82664897655536a46676714f576960bca4a59b7f
CosmicLatte009 <ohhm009@gmail.com>
2022-06-02 22:03:00 +0900

    013_checkPrime push

---
+++ 013_checkPrime.js
@@ -0,0 +1,23 @@
+// 소수판별 (소수: 1과 자기 자신으로만 나눠지는 수)
+// 소수이면 Yes, 아니면 No로 출력하세요.
+
+// 소수판별 (소수: 1과 자기 자신으로만 나눠지는 수)
+// 소수이면 Yes, 아니면 No로 출력하세요.
+
+function checkPrime(n) {
+    for (i = 2; i < n; i++) {
+        if (n % i === 0) {
+            console.log("NO");
+            return false;
+        }
+    }
+
+    if (n === 1) {
+        console.log("NO");
+        return false;
+    }
+    console.log("YES")
+}
+
+console.log(8);
+checkPrime(8);
